a02dbb3b5706e7abe58203b11b57d50ea3a32fb5
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-09-29 12:31:38 +0300

    added file App and Profile

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Profile from './Profile'; 
+
+const user = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+};
+
+function App() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
