14828074ccea7309ab69b93bedc9ea762a4adadd
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-10-02 19:48:00 +0300

    added styles

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import styles from './Notification.module.css'
+
 const Notification = ({ message }) => {
-  return <p>{message}</p>;
+  return <p className={styles.p}>{message}</p>;
 };
 
 export default Notification;
